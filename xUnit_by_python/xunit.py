8a837362a2287a6513d2a62f9d359759e9396662
Arinobu Fukuhara <afukuhara@gmail.com>
2024-08-17 11:56:56 +0900

    finish chapter 22

--- xUnit_by_python/xunit.py
+++ xUnit_by_python/xunit.py
@@ -13,8 +13,12 @@ class TestCase:
         result.testStarted()
 
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:    
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
+
         self.tearDown()
 
         return result
@@ -23,13 +27,16 @@ class TestCase:
 class TestResult:
     def __init__(self):
         self.runCount = 0
-        self.failCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount += 1
 
+    def testFailed(self):
+        self.errorCount += 1
+
     def summary(self):
-        return f"{self.runCount} run, {self.failCount} failed"
+        return f"{self.runCount} run, {self.errorCount} failed"
 
 
 class WasRun(TestCase):
@@ -62,7 +69,14 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-# TestCaseTest("testFailedResult").run()
+
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
